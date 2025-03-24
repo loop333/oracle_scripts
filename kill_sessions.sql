3ea5f6d32b8c050862960f9738e82e101f92ac19
Dmitry <37012903+loop333@users.noreply.github.com>
2025-03-24 14:38:27 +0500

    Update kill_sessions.sql

--- kill_sessions.sql
+++ kill_sessions.sql
@@ -5,13 +5,13 @@ for c in (
 select sid, serial# from gv$session s, dba_procedures p where program like '%PRG_NAME%'
 and p.object_id = s.plsql_entry_object_id and p.subprogram_id = s.plsql_entry_subprogram_id
 and p.procedure_name = 'PROC_NAME') loop
- execute immediate 'alter system kill session '''||c.sid||','||c.serial#||''' immediate;';
+ execute immediate 'alter system kill session '''||c.sid||','||c.serial#||''' immediate';
 end loop;
 end
 
 begin
 for c in (select sid, serial# from gv$session s where s.user# = 0 and s.machine like 'machine%') loop
- execute immediate 'alter system kill session '''||c.sid||','||c.serial#||''' immediate;';
+ execute immediate 'alter system kill session '''||c.sid||','||c.serial#||''' immediate';
 -- null;
 end loop;
 end;
